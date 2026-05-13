053453dc3e7cffa280bc4843d728aaa16ccdac3b
mahendra <veeramahendrareddymedam@gmail.com>
2026-05-13 11:50:58 +0530

    added session cart and remove feature

--- cart.php
+++ cart.php
@@ -1,13 +1,24 @@
 <?php
+session_start();
 include "db.php";
+if(isset($_GET['remove'])){
+
+    $key = $_GET['remove'];
+
+    unset($_SESSION['cart'][$key]);
+
+    header("Location: cart.php");
+}
 
 if(isset($_GET['id'])){
 
     $id = $_GET['id'];
 
-    $result = mysqli_query($conn, "SELECT * FROM products WHERE id=$id");
+    if(!isset($_SESSION['cart'])){
+        $_SESSION['cart'] = [];
+    }
 
-    $product = mysqli_fetch_assoc($result);
+    $_SESSION['cart'][] = $id;
 }
 ?>
 
@@ -18,17 +29,44 @@ if(isset($_GET['id'])){
 </head>
 <body>
 
-<h1>Cart Page</h1>
+<h1>Shopping Cart</h1>
 
-<?php if(isset($product)) { ?>
+<?php
 
-<h2><?php echo $product['name']; ?></h2>
+$total = 0;
 
-<p>Price: ₹<?php echo $product['price']; ?></p>
+if(isset($_SESSION['cart'])){
 
-<img src="images/<?php echo $product['image']; ?>" width="200">
+    foreach($_SESSION['cart'] as $key => $product_id){
 
-<?php } ?>
+        $result = mysqli_query($conn, "SELECT * FROM products WHERE id=$product_id");
+
+        $product = mysqli_fetch_assoc($result);
+
+        $total += $product['price'];
+?>
+
+<div style="border:1px solid black;padding:10px;margin:10px;">
+
+    <h3><?php echo $product['name']; ?></h3>
+
+    <p>₹<?php echo $product['price']; ?></p>
+
+    <img src="images/<?php echo $product['image']; ?>" width="150">
+    <br><br>
+
+<a href="cart.php?remove=<?php echo $key; ?>">
+    <button>Remove</button>
+</a>
+
+</div>
+
+<?php
+    }
+}
+?>
+
+<h2>Total: ₹<?php echo $total; ?></h2>
 
 </body>
 </html>
